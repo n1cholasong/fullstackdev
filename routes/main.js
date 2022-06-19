6261a10c85b71331dd9ce5a01562a52bceeb7ac0
Nicholas Ong <nicholasong75@gmail.com>
2022-06-20 04:54:52 +0800

    minor tweak

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res,) => {
 	// renders views/index.handlebars, passing title as an object
-	res.render('index')
+	res.render('index');
 });
 
 router.get('/mycourse', (req, res) => {
