12b591355a13214ce34f9b72ea8398467260ba98
Nicholas Ong <nicholasong75@gmail.com>
2022-06-19 18:41:51 +0800

    updated header

--- routes/main.js
+++ routes/main.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	const title = 'Video Jotter';
+	const title = 'Curosa';
 	// renders views/index.handlebars, passing title as an object
 	res.render('index', { title: title })
 });
 
-router.get('/about', (req, res) => {
-	const author = 'Your Name';
-	res.render('about', { author });
+router.get('/mycourse', (req, res) => {
+	res.render('mycourse');
 });
 
 module.exports = router;
