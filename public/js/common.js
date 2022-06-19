f0f30b89d444b2e3712933ef82647936760c8c1c
Kit-master <Kiat0878@gmail.com>
2022-06-19 13:58:51 +0800

    Debug done

--- public/js/common.js
+++ public/js/common.js
@@ -10,3 +10,20 @@ menuIconButton.addEventListener("click", () => {
 //    $(".sidebar .nav-item").find(".active").removeClass("active");
 //    $(this).addClass("active");
 // });
+
+$(function(){
+   var current = location.pathname;
+   console.log( $('#nav li') )
+   // var nav = $('#nav li ')
+   // for (i=0;i<=nav.length;i++){
+   //    console.log(nav[i].attr('href') )
+   // }
+   $('#nav li ').each(function(){
+       var $this = $(this);
+       console.log($this.attr('id'))
+       // if the current path is like this link, make it active
+       if("/"+$this.attr('id') == current){
+           $this.addClass('active');
+       }
+   })
+})
