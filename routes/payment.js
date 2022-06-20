62da0b01268e3dc5e30cd0a16e58812c5db74ac7
Kit-master <Kiat0878@gmail.com>
2022-06-20 23:29:04 +0800

    Started playing around with payment posting

--- routes/payment.js
+++ routes/payment.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	const title = 'Curosa';
 	// renders views/index.handlebars, passing title as an object
 	res.render('./payment/payment' )
 });
+
+router.post('/',(req,res)=>{
+let name = req.body.name;
+let cardNumber = req.body.cardDetails1 + req.body.cardDetails2 + req.body.cardDetails3 + req.body.cardDetails4;
+
+console.log(name)
+
+})
 
 
 module.exports = router;
