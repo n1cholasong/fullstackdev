42ebc3528360b10f071b8d36ad4a4b78a5299b85
N4thanael <leenathanaelnoah@gmail.com>
2022-06-21 16:38:32 +0800

    No more pls

--- routes/forum.js
+++ routes/forum.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Forum = require('../models/Forum');
+require('dotenv').config;
 
-router.get("/forum", (req, res) => {
-    res.render('forum/forumhome')
+router.get("/", (req, res) => {
+    Forum.findAll({
+        raw: true
+    })
+        .then((thread) => {
+            res.render('forum/forumhome', { thread });
+        })
+        .catch(err => console.log(err));
+
 });
 
-router.post("/createThread", (req, res) =>{
+router.post("/createThread", (req, res) => {
     let topic = req.body.topic;
-    let description = req.body.description;
+    let description = req.body.thread_description;
 
-    res.redirect('/forum/forum')
+    Forum.create(
+        {
+            topic, description
+        }
+    )
+
+    res.redirect('/forum/')
 })
 
 module.exports = router;
