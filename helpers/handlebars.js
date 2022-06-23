ed48a885c1ebb871378d46d45cdb4bb9e9913b0e
Xepvoid <97590001+Xepvoid@users.noreply.github.com>
2022-06-23 15:16:13 +0800

    idk

---
+++ helpers/handlebars.js
@@ -0,0 +1,16 @@
+const moment = require('moment');
+const formatDate = function (date, targetFormat) {
+    return moment(date).format(targetFormat);
+};
+
+const replaceCommas = function (value) {
+    return value ? value.replace(/,/g, ' | ') : 'None';
+}
+
+const checkboxCheck = function (value, checkboxValue) {
+    return (value.search(checkboxValue) >= 0) ? 'checked' : '';
+};
+const radioCheck = function (value, radioValue) {
+    return (value == radioValue) ? 'checked' : '';
+};
+module.exports = { formatDate, replaceCommas, checkboxCheck, radioCheck }
