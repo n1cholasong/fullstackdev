64abee9d4f6ee5820b96df26616856667c029cfd
Kit-master <Kiat0878@gmail.com>
2022-06-23 17:24:22 +0800

    Added in helpers and improved edit date UI

---
+++ helpers/handlebars.js
@@ -0,0 +1,19 @@
+const moment = require('moment')
+
+const formatDate = function (date,targetFormat) {
+    return moment(date).format(targetFormat)
+}
+
+const replaceCommas = function(value){
+    return value ? value.replace(/,/g,' | ') : 'None';
+}
+
+const checkboxCheck = function(value,checkboxValue){
+    return (value.search(checkboxValue) >= 0) ? 'checked' : '';
+}
+
+const radioCheck = function(value,radioValue){
+    return (value == radioValue) ? 'checked' : '';
+}
+
+module.exports= {formatDate,replaceCommas,checkboxCheck,radioCheck}
