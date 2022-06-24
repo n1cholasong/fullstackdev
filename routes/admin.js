1a1a16409751a0b8bbe3ffefb7b45f8a4e2df6df
Nicholas Ong <nicholasong75@gmail.com>
2022-06-25 04:17:37 +0800

    account CRUD complete!!!

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 const ensureAuthenticated = require("../helpers/auth");
 
-router.get('/manageAccounts', ensureAuthenticated, (req, res) => {
+router.get('/manageAccounts', (req, res) => {
     User.findAll({
         raw: true
     })
@@ -11,6 +11,23 @@ router.get('/manageAccounts', ensureAuthenticated, (req, res) => {
             res.render('./admin/accountManagement', { users });
         })
         .catch((err) => console.log(err));
+});
+
+router.post('/deleteAccount/:id', ensureAuthenticated, async function (req, res) {
+    try {
+        let user = await User.findByPk(req.params.id);
+        if (!user) {
+            res.redirect('../../user/manageAccounts/');
+            return;
+        }
+
+        let result = await User.destroy({ where: { id: user.id } });
+        console.log(result + ' account deleted');
+        res.redirect('../../user/manageAccounts/');
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
