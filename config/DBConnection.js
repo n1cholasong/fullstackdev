237606c9035b7399d12a78256bcd74e53c8a634f
Kit-master <Kiat0878@gmail.com>
2022-06-30 16:08:05 +0800

    Added in relationships and created more DB

--- config/DBConnection.js
+++ config/DBConnection.js
@@ -2,6 +2,9 @@ const mySQLDB = require('./DBConfig');
 const User = require('../models/User')
 const Review = require('../models/Review')
 const Forum = require('../models/Forum');
+const Course = require('../models/Courses')
+const Voucher = require('../models/Voucher')
+
 
 // If drop is true, all existing tables are dropped and recreated 
 const setUpDB = (drop) => { 
@@ -11,6 +14,11 @@ const setUpDB = (drop) => {
         /* Defines the relationship where a user has many videos. The primary key from user will be a foreign key in video. */ 
         User.hasMany(Forum);
         Forum.belongsTo(User);
+        User.hasMany(Course);
+        User.hasMany(Voucher);
+        Voucher.belongsToMany(User,{through: 'UserVouchers'})
+        Course.belongsToMany(User,{through: 'UserCourses'});
+       // Course.belongsTo(User)
         mySQLDB.sync({ 
             force: drop 
         }); }) 
