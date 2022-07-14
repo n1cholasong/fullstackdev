255abc7df0219729fa18cdc3ac911e4484030bc6
n1ch0las.ong <nicholasong75@gmail.com>
2022-07-15 00:13:13 +0800

    updated user profile

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const { Sequelize } = require('sequelize');
 const Seqelize = require('sequelize');
 const db = require('../config/DBConfig')
 
@@ -5,6 +6,13 @@ const User = db.define('user', {
     email: { type: Seqelize.STRING },
     username: { type: Seqelize.STRING },
     password: { type: Seqelize.STRING },
+    fname: { type: Seqelize.STRING },
+    lname: { type: Seqelize.STRING },
+    gender: { type: Seqelize.STRING },
+    birthday: {type: Seqelize.DATEONLY},
+    country: { type: Seqelize.STRING },
+    interest: { type: Seqelize.STRING },
+    status: {type: Sequelize.STRING(300)},
     role: { type: Seqelize.STRING }
 });
 
