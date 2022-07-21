d81eeaee28e1a932b64e9794317a1feb773a19f7
Kit-master <Kiat0878@gmail.com>
2022-07-21 21:55:56 +0800

    Improved UI for quiz and fixed sidebar create course link

--- public/js/Quiz.js
+++ public/js/Quiz.js
@@ -1,3 +1,8 @@
-function checkans(optionNum) {
-document.getElementById("cans").value = optionNum;
+function checkans(obj) {
+    $('.ansBtn').each(function(){
+        $(this).css("background-color","");
+    })
+
+    document.getElementById("cans").value = parseInt(obj.id.split("ansBtn")[1]);
+    obj.style = "background-color: #7560FF;"
 }
