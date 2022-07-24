0e8ac07bc346ccc2bd9fc500773bcf627cafca11
Kit-master <Kiat0878@gmail.com>
2022-07-24 18:35:03 +0800

    Combineing quizes and video into chapters

--- public/js/common.js
+++ public/js/common.js
@@ -122,3 +122,27 @@ $('#pictureUpload').on('change', function () {
          }
       })
 });
+
+
+// Use fetch to call post route /video/upload
+$('#videoUpload').on('change', function () {
+   let formdata = new FormData();
+   let image = $("#videoUpload")[0].files[0];
+   formdata.append('videoUpload', image);
+   fetch('/Course/upload', {
+      method: 'POST',
+      body: formdata
+   })
+      .then(res => res.json())
+      .then((data) => {
+         $('#picture').attr('src', data.file);
+         $('#pictureURL').attr('value', data.file); // sets posterURL hidden field
+         if (data.err) {
+            $('#pictureErr').show();
+            $('#pictureErr').text(data.err.message);
+         }
+         else {
+            $('#pictureErr').hide();
+         }
+      })
+});
