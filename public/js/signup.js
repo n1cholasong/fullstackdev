94c188d66b08ba06eaa9333b32651fc5e54f7ba5
Nicholas Ong <nicholasong75@gmail.com>
2022-07-26 09:16:33 +0800

    updated multistep form

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,39 +1,37 @@
-const multiStepForm = document.querySelector("[data-multi-step]");
-const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
 let currentStep = formSteps.findIndex(step => {
     return step.classList.contains("active")
-});
+})
 
-console.log(currentStep);
-
-if (currentStep) {
-    currentStep = 0;
+if (currentStep < 0) {
+    currentStep = 0
     formSteps[currentStep].classList.add("active");
-};
+}
 
 multiStepForm.addEventListener("click", e => {
-    if (e.target.matches("[data-next")) {
+    let incrementor
+    if (e.target.matches("[data-next]")) {
         incrementor = 1
     } else if (e.target.matches("[data-back]")) {
         incrementor = -1
-    } else {
-        return;
-    };
-    
-    if (incrementor == null) return;
-    const inputs = [...formSteps[currentStep].querySelectorAll(":input")];
-    const allValid = inputs.some(input => input.checkValidity());
+    } 
+
+    if (incrementor == null) return
+
+    const inputs = [...formSteps[currentStep].querySelectorAll("input, select")];
+    const allValid = inputs.every(input => input.reportValidity())
+    console.log(inputs)
     if (allValid) {
-        currentStep += incrementor;
-        showCurrentStep();
+        currentStep += incrementor
+        showCurrentStep()
     };
 
-    showCurrentStep();
-});
+    showCurrentStep()
+})
 
 function showCurrentStep() {
     formSteps.forEach((step, index) => {
         step.classList.toggle("active", index === currentStep)
     })
-};
-console.log(currentStep);
+}
