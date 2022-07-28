49338a8e0db457d33dd32edd71bb593a91711329
Kit-master <Kiat0878@gmail.com>
2022-07-28 15:11:12 +0800

    I don't know

--- public/js/viewChapter.js
+++ public/js/viewChapter.js
@@ -3,10 +3,18 @@ function hide(vNum,maxNum) {
     {
         var x = document.getElementById("videoPlayer" + i)
         x.style.display = "none"
+        x = document.getElementById("quiz" + i)
+        x.style.display = "none"
     }
 
 
     var x = document.getElementById("videoPlayer" + vNum);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    x = document.getElementById("quiz" + vNum);
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
