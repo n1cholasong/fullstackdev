2d2f8d4831a692337df88423bf5dd772182b9ed7
Kit-master <Kiat0878@gmail.com>
2022-08-05 14:46:38 +0800

    Did some things can't rember

--- public/js/viewChapter.js
+++ public/js/viewChapter.js
@@ -1,23 +1,32 @@
 function hide(vNum,maxNum) {
     for(var i = 0;i<maxNum;i++)
     {
+        var y = document.getElementById("quiz" + i)
+        y.style.display = "none"
+        try{
         var x = document.getElementById("videoPlayer" + i)
         x.style.display = "none"
-        x = document.getElementById("quiz" + i)
-        x.style.display = "none"
+        }catch{
+          continue
+        }
     }
 
 
+    var y = document.getElementById("quiz" + vNum);
+    if (y.style.display === "none") {
+      y.style.display = "";
+    } else {
+      y.style.display = "none";
+    }
+    try{
     var x = document.getElementById("videoPlayer" + vNum);
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-    x = document.getElementById("quiz" + vNum);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+   }
+   catch{
+    console.log("I DON'tCARE")
+   }
+  } 
