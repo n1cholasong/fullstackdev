f5d28a585678e2cc0d6a70bbcb4ba6984883c6d5
Kit-master <Kiat0878@gmail.com>
2022-08-14 14:03:20 +0800

    UI improvement

--- models/Quizes.js
+++ models/Quizes.js
@@ -1,5 +1,10 @@
 const Seqelize = require('sequelize');
 const db = require('../config/DBConfig');
+const Chapter = require('./chapter');
+
+async function findChapterId(){
+   return await Chapter.findAll({raw:true}).then((chapter) => {return chapter})
+}
 
 const Quizes = db.define('Quizes', {
     question: { type: Seqelize.STRING },
@@ -9,6 +14,42 @@ const Quizes = db.define('Quizes', {
     a3: { type: Seqelize.STRING},
     a4: { type: Seqelize.STRING},
     correctans: { type: Seqelize.STRING}
+});
+
+Quizes.sync();
+Chapter.sync();
+
+
+Quizes.findAndCountAll()
+.then(async function(result) {
+    try{
+    if (result.count < 1) {
+        
+        await Quizes.create({
+            question: "What is python",
+            description: "fist question",
+            a1: "A progmaming laungague",
+            a2: "A Type of Snake",
+            a3: "Anaconda?",
+            a4: "I don't know",
+            correctans: "A progmaming laungague",
+            ChapterId: 1
+        })
+        
+    };
+    }
+    catch(err){
+        await Quizes.create({
+            question: "What is python",
+            description: "fist question",
+            a1: "A progmaming laungague",
+            a2: "A Type of Snake",
+            a3: "Anaconda?",
+            a4: "I don't know",
+            correctans: "A progmaming laungague",
+            ChapterId: 2
+        })
+    }
 });
 
 module.exports = Quizes
