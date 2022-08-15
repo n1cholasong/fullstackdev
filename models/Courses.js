79f1a877dd4d73278a4c634875854e6379c64fc8
Nicholas Ong <nicholasong75@gmail.com>
2022-08-15 12:15:08 +0800

    category css

--- models/Courses.js
+++ models/Courses.js
@@ -5,7 +5,7 @@ const User = require('./User');
 const Courses = db.define('Courses', {
     courseName: { type: Seqelize.STRING },
     description: { type: Seqelize.STRING },
-    content: { type: Seqelize.STRING}
+    content: { type: Seqelize.STRING }
 });
 
 Courses.sync();
@@ -13,7 +13,7 @@ User.sync();
 
 coursesList = [{
     courseName: "Python 101", description: "Basic Python knowdlge", content: "Basic Python knowdlge", userId: 1
-},{
+}, {
     courseName: "Programing 101", description: "Basic knowdlge on C# python etc", content: "learn how to print Hello world", userId: 2
 },
 {
@@ -24,15 +24,15 @@ coursesList = [{
 Courses.findAndCountAll()
     .then(async result => {
         try {
-        if (result.count < 1) {
-            coursesList.forEach(async function(course) {
-                await Courses.create(course)
-            })
-            
-        };
-    }catch(err){
-        console.log(err)
-    }
+            if (result.count < 1) {
+                coursesList.forEach(async function (course) {
+                    await Courses.create(course)
+                })
+
+            };
+        } catch (err) {
+            console.log(err)
+        }
     });
 
 module.exports = Courses
