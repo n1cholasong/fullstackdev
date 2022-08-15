876c695a30ad05d1462e46f70c9f60b7e47ac840
Kit-master <Kiat0878@gmail.com>
2022-08-15 16:29:10 +0800

    FIxed the Quiz issue as well as added in categrroy

--- models/Quizes.js
+++ models/Quizes.js
@@ -16,31 +16,5 @@ const Quizes = db.define('Quizes', {
     correctans: { type: Seqelize.STRING}
 });
 
-Quizes.sync();
-Chapter.sync();
-
-
-Quizes.findAndCountAll()
-.then(async function(result) {
-    try{
-    if (result.count < 1) {
-        
-        await Quizes.create({
-            question: "What is python",
-            description: "fist question",
-            a1: "A progmaming laungague",
-            a2: "A Type of Snake",
-            a3: "Anaconda?",
-            a4: "I don't know",
-            correctans: "A progmaming laungague",
-            ChapterId: 1
-        })
-        
-    };
-    }
-    catch(err){
-       console.log(err)
-    }
-});
 
 module.exports = Quizes
